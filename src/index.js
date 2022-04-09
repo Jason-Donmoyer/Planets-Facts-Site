71b52346174fa3b9a7d5fe4a82e3873024dbd1d3
Jason-Donmoyer <j.donmoyer523@gmail.com>
2022-04-09 11:46:52 -0400

    Added files and functionality: -HTML: --added button to index.html to test routes -CSS: --added normalize.css and styles.css -JS: --added main.js for front-end styles --imported node-fetch --set up fetch call to get data from API

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import fetch from 'node-fetch';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,8 +12,21 @@ const __dirname = dirname(__filename);
 const app = express();
 const router = express.Router();
 
+const data = [];
+
 app.use(cors());
 // app.use(express.static('public'));
+
+
+// Get data from API
+fetch('https://jason-donmoyer.github.io/JSON-Files/planet-data.json')
+  .then(res => res.json())
+  .then(planets => {
+    // console.log(planets[4].name);
+    for (let i in planets) {
+      console.log(planets[i].name);
+    }
+  });
 
 
 // ROUTES
