4f24d852ccfe51c12a995bea9f374332197c8537
Jason-Donmoyer <j.donmoyer523@gmail.com>
2022-04-16 05:30:43 -0400

    testing localStorage

--- src/public/main.js
+++ src/public/main.js
@@ -88,9 +88,11 @@ async function getData() {
     }
     // let planetNumber = 0;
     console.log(planetData[planetNumber].overview.content);
+    localStorage.setItem('planetData', planetData);
   });
 }
 
 getData();
+localStorage.getItem('planetData');
 
 
