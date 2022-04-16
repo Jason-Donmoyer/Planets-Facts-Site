2574650b7c057247902a7301237d8519b8d79805
Jason-Donmoyer <j.donmoyer523@gmail.com>
2022-04-16 05:26:51 -0400

    Added functionality to nav: -HTML: --updated all pages with index.html template -- ISSUE -- pages will need to be dynamically updated -JS: --created array for nav btns --created links to all pages from nav btns

--- src/public/main.js
+++ src/public/main.js
@@ -3,8 +3,12 @@
 const hamburgerMenuBtn = document.getElementById('hamburger-menu');
 const navBtnsContainer = document.getElementById('nav-btns-container');
 const mainContentContainer = document.getElementById('main-content');
+let planetNumber;
 
 const planetData = [];
+
+// Check to see if planet number is defined in localStorage
+localStorage.getItem('planetNumber') ? planetNumber = localStorage.getItem('planetNumber') : planetNumber = 0;
 
 
 // Hamburger Menu Functions
@@ -30,7 +34,50 @@ hamburgerMenuBtn.addEventListener('click', () => {
   
 });
 
+// Add event listenter to nav Buttons
 
+const mercuryNavBtn = document.getElementById('mercury-nav-btn');
+const venusNavBtn = document.getElementById('venus-nav-btn');
+const earthNavBtn = document.getElementById('earth-nav-btn');
+const marsNavBtn = document.getElementById('mars-nav-btn');
+const jupiterNavBtn = document.getElementById('jupiter-nav-btn');
+const saturnNavBtn = document.getElementById('saturn-nav-btn');
+const uranusNavBtn = document.getElementById('uranus-nav-btn');
+const neptuneNavBtn = document.getElementById('neptune-nav-btn');
+
+// Array to hold nav buttons
+const navBtnsArr = [
+  mercuryNavBtn,
+  venusNavBtn,
+  earthNavBtn,
+  marsNavBtn,
+  jupiterNavBtn,
+  saturnNavBtn,
+  uranusNavBtn,
+  neptuneNavBtn
+];
+
+// Add event listeners and store planet number to extract data from API
+for (let i = 0; i < navBtnsArr.length; i++) {
+  // console.log(navBtnsArr[i].children[0].children[1].textContent.toLowerCase());
+  navBtnsArr[i].addEventListener('click', () => {
+    planetNumber = i;
+    localStorage.setItem('planetNumber', planetNumber);
+    console.log(planetNumber);
+    const link = document.createElement('a');
+      if (`/${navBtnsArr[i].children[0].children[1].textContent.toLowerCase()}` === '/mercury') {
+        link.href = '/';
+      } else {
+        link.href = `/${navBtnsArr[i].children[0].children[1].textContent.toLowerCase()}`;
+      }   
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+  })
+}
+
+
+// Get data from API
 async function getData() {
   const res = await fetch('http://localhost:3000/data')
   .then(res => res.json())
@@ -39,7 +86,7 @@ async function getData() {
     for (let i in data) {
       planetData.push(data[i]);
     }
-    let planetNumber = 0;
+    // let planetNumber = 0;
     console.log(planetData[planetNumber].overview.content);
   });
 }
